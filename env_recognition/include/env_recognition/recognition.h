481d404057efc8b57cee93c8587548f71edc35e2
Thanos Manolis <thanos.m14@gmail.com>
2018-12-22 20:16:02 +0200

    Change way of density detection and average value calculation

--- env_recognition/include/env_recognition/recognition.h
+++ env_recognition/include/env_recognition/recognition.h
@@ -16,19 +16,25 @@ class Recognition
 		ros::NodeHandle n_;
 		std_msgs::String msg_env_;	//! Publish the recognised environment through this msg
 
+		//! Random variables
 		int ranges_size_;			//! Size of ranges[] list
-		int temp_var_;				//! Variance of each measurement				
-		int sum_vars_;				//! Sum of variances so far
-		float average_var_;			//! Mean value of variance of each measurement
-		int start_;					//! Start point of most recent feature
-		int end_;					//! End point of most recent feature
-		int temp_features_;			//! Sum of all features' of each measurement
-		int sum_features_;			//! Sum of all features' sizes so far
-		float average_features_;	//! Mean value of all features' sizes of each measurement
+
+		int temp_var_;				//! Variance of each measurement	
+		int list_vars_[100];		//! A list of the last 100 variances			
+		int sum_vars_;				//! Sum of the last 100 variances
+		float average_var_;			//! Mean value of the last 100 variances
+		
+		int temp_features_;			//! Non-infinite ranges of each measurement
+		int list_features_[100];	//! A list of the last 100 non-ininite ranges		
+		int sum_features_;			//! Sum of the last 100 non-ininite ranges
+		float average_features_;	//! Mean value of the last 100 non-ininite ranges
+
 		int counter_msgs_;			//! Counts the laser measuremets so far
 		int counter_inf_;			//! Counts the infinite values at each measurement
-		string temp_env_;			//! Environment type to be published
+		int index_;					//! Index of the list we are working on 
+		int samples_; 				//! Samples, from which average value will occur
 		int case_;					//! Environment case we are at
+		string temp_env_;			//! Environment type to be published
 		
 		//! ROS Parameters
 		int threshold_;				//! Lower -> Walls  || Higher -> Features (maybe with walls)
@@ -41,10 +47,10 @@ class Recognition
 		ros::Subscriber scan_sub_;
 
 		//! Random Functions
-		void laserCallback			(const sensor_msgs::LaserScan::ConstPtr &msg_laser);
-		void varianceAverageValue	(void);
-		void initParams 		    (void);
-		void initValues				(void);
+		void laserCallback	(const sensor_msgs::LaserScan::ConstPtr &msg_laser);
+		void averageValues	(void);
+		void initParams 	(void);
+		void initValues		(void);
 
 	public:
 		Recognition 	(void);
--- env_recognition/src/recognition.cpp
+++ env_recognition/src/recognition.cpp
@@ -13,6 +13,7 @@ Recognition::Recognition (void)
 {
 	initParams();
 	initValues();
+
 	scan_sub_ = n_.subscribe("/scan", 10, &Recognition::laserCallback, this);
 	env_pub_ = n_.advertise<std_msgs::String>("/environment", 10);
 }
@@ -29,9 +30,31 @@ Recognition::~Recognition (void)
 }
 
 /*
-************************************
-*    Initialize the parameters.    *  
-************************************
+*****************************************
+*    Initialize the necessary values    *  
+*****************************************
+*/
+
+void Recognition::initValues (void)
+{
+	counter_msgs_ = 0;
+	sum_vars_ = 0;
+	sum_features_ = 0;
+	temp_env_ = ""; 
+	index_ = 0;
+	samples_ = 0;
+
+	for(int i; i<100; i++)
+	{
+		list_vars_[i] = 0;
+		list_features_[i] = 0;
+	}
+}
+
+/*
+***********************************
+*    Initialize the parameters    *  
+***********************************
 */
 
 void Recognition::initParams (void)
@@ -53,49 +76,32 @@ void Recognition::initParams (void)
 	}  
 	else 
 	{
-		ROS_WARN("[Parameters] Parameter threshold not found.\
+		ROS_WARN("[Parameters] Parameter threshold_density not found.\
 			Using Default");
-		threshold_density_ = 40;
+		threshold_density_ = 40.0;
 	}
 }
 
 /*
-******************************************
-*    Initialize the necessary values.    *  
-******************************************
-*/
-
-void Recognition::initValues (void)
-{
-	sum_vars_ = 0;
-	sum_features_ = 0;
-	counter_msgs_ = 0;
-	temp_env_ = ""; 
-}
-
-/*
-*************************************
-*    Get data from laser sensor.    *  
-*************************************
+************************************
+*    Get data from laser sensor    * 
+************************************
 */
 
 void Recognition::laserCallback (const sensor_msgs::LaserScan::ConstPtr &msg_laser)
 {
 	ranges_size_ = msg_laser->ranges.size();
+	temp_env_ = ""; 
 	temp_var_ = 0;
 	temp_features_ = 0;
 	counter_inf_ = 0;
-	ROS_INFO("*************************************************");
-	for(int i=2; i<(msg_laser->ranges.size()-1); i++)
-	{
 
-		/*
-		******************************************************************
-		*    Search for big changes between 2 ranges, where a wall,      *
-	    *    a feature or a door could end/start. If one is detected,    *
-	    *	 then raise temp_var (temporary variance) by 1.			 *
-		******************************************************************
-		*/
+	ROS_INFO("*************************************************");
+	for(int i=1; i<ranges_size_; i++)
+	{
+		//! Search for big changes between 2 ranges, where a wall,     
+	    //! a feature or a door could end/start. If one is detected,    
+	    //! then raise temp_var (temporary variance) by 1		 
 		if((msg_laser->ranges[i] > (1.35)*msg_laser->ranges[i-1]) || (msg_laser->ranges[i] < (0.65)*msg_laser->ranges[i-1]))
 		{			
 			if((msg_laser->ranges[i-1] > msg_laser->range_max) && (msg_laser->ranges[i] > msg_laser->range_max))
@@ -105,21 +111,7 @@ void Recognition::laserCallback (const sensor_msgs::LaserScan::ConstPtr &msg_las
 			{
 				ROS_INFO("*ranges[%d]: %f --- ranges[%d]: %f", i, msg_laser->ranges[i], i-1, msg_laser->ranges[i-1]);
 				temp_var_++;
-
-				//Calculate size of each feature, and add it to sum of all features' sizes
-				if((msg_laser->ranges[i] < (0.75)*msg_laser->ranges[i-1]))
-				{
-					start_ = i;
-				}else if((msg_laser->ranges[i] > (1.35))*msg_laser->ranges[i-1])
-				{
-					if(start_ < msg_laser->ranges[i])
-						end_ = i;
-						temp_features_ += end_ - start_;
-				}
 			}
-		}else
-		{
-
 		}
 
 		//! Count infinite values
@@ -130,7 +122,7 @@ void Recognition::laserCallback (const sensor_msgs::LaserScan::ConstPtr &msg_las
 	}
 
 	//! If the robot sees only inf values, then it sees nothing
-	if(counter_inf_ == msg_laser->ranges.size()-3)
+	if(counter_inf_ == ranges_size_-1)
 	{
 		if(case_ != 0)
 		{
@@ -142,42 +134,54 @@ void Recognition::laserCallback (const sensor_msgs::LaserScan::ConstPtr &msg_las
 		}
 	}else
 	{
-		varianceAverageValue();
+		temp_features_ = ranges_size_- 1 - counter_inf_; 
+		averageValues();
 	}
 }
 
 /*
-***************************************************
-*    Calculate the average variance until now.    *  
-***************************************************
+************************************************
+*    Calculate the average values until now    *  
+************************************************
 */
 
-void Recognition::varianceAverageValue (void)
+void Recognition::averageValues (void)
 {
+	//! For the first 100 messages, raise the samples_ by 1. Then, keep it to 100
+	//! Also, reset the index of the list (index_), everytime it reaches 100
+	if(counter_msgs_%100 == 0)
+		index_ = 0;
+	if(counter_msgs_ < 100)
+		samples_++;
 	counter_msgs_++;
-	//! Every 500 messages, reset average variance
-	if(counter_msgs_%500 == 0)
-	{	
-		counter_msgs_ = 0;
-		sum_vars_ = 0;
-		sum_features_ = 0;
-	}
+
+	//! Temp variable to store the element of the list that is going to be overriden
+	int previous;
 
 	//! Calculate average variance so far
-	sum_vars_ += temp_var_;
-	average_var_ = (float)sum_vars_ / ((float)counter_msgs_);
+	previous = list_vars_[index_];
+	list_vars_[index_] = temp_var_;
+	sum_vars_ += list_vars_[index_] - previous;
+	average_var_ = (float)sum_vars_ / samples_;
 
 	//! Calculate average features' sizes so far
-	sum_features_ += temp_features_;
-	average_features_ += (float)sum_features_ / ((float)counter_msgs_);
+	previous = list_features_[index_];
+	list_features_[index_] = temp_features_;
+	sum_features_ += list_features_[index_] - previous;
+	average_features_ = (float)sum_features_ / samples_;
 
 	ROS_INFO("*[threshold_] %d", threshold_);
-	ROS_INFO("*[counter_msgs_] %d", counter_msgs_);
+	ROS_INFO("*[threshold_density_] %d", threshold_density_);
+	ROS_INFO("*[index_] %d", index_);
+	ROS_INFO("*[samples_] %d", samples_);
 	ROS_INFO("*[temp_var_] %d", temp_var_);
 	ROS_INFO("*[average_var_] %f", average_var_);
 	ROS_INFO("*[temp_features_] %d", temp_features_);
 	ROS_INFO("*[average_features_] %f", average_features_);
 	ROS_INFO("*************************************************");
+	
+	//! Raise the index
+	index_++;
 
 	if((average_var_ <= threshold_ && average_var_ >= 0))
 	{
@@ -190,7 +194,6 @@ void Recognition::varianceAverageValue (void)
 			else
 				temp_env_ = "|| Walls: Yes   || Features: No  || Density: Sparse ||";
 			
-			ROS_INFO("[average_var_] %f", average_var_);
 			msg_env_.data = temp_env_.c_str();
 			env_pub_.publish(msg_env_);
 		}
@@ -205,7 +208,6 @@ void Recognition::varianceAverageValue (void)
 			else
 				temp_env_ = "|| Walls: Maybe || Features: No  || Density: Sparse ||";
 			
-			ROS_INFO("[average_var_] %f", average_var_);
 			msg_env_.data = temp_env_.c_str();
 			env_pub_.publish(msg_env_);
 		}
